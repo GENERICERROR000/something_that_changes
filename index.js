3edd04c0a01550d672f6f858f18e20856ceb60bc
Noah Kernis <nkernis@gmail.com>
2020-10-24 16:18:42 -0400

    color bg and organized assets

--- index.js
+++ index.js
@@ -64,8 +64,10 @@ var ALARM_SPEED
 
 var clockFont
 var blurShader
-var clockCanvas
-var textCanvas
+var noiseShader
+var timeHolderCanvas
+var timeCanvas
+var noiseHolderCanvas
 
 // clock parameters
 const clockFace = {
@@ -86,11 +88,16 @@ const clockFace = {
 // })
 
 function preload() {
-	// load shader
-	blurShader = loadShader('blur.vert', 'blur.frag')
+	// load shaders
+
+	// source: https://github.com/aferriss/p5jsShaderExamples/blob/gh-pages/4_image-effects/4-9_single-pass-blur/effect.frag
+	blurShader = loadShader('assets/shaders/blur/blur.vert', 'assets/shaders/blur/blur.frag')
+
+	// source: https://glitch.com/~recursive-noise-experiment
+	noiseShader = loadShader('assets/shaders/noise/noise.vert', 'assets/shaders/noise/noise.frag')
 
 	// load font
-	clockFont = loadFont("digital_7_mono.ttf")
+	clockFont = loadFont("assets/fonts/digital_7_mono.ttf")
 }
 
 function setup() {
@@ -98,13 +105,22 @@ function setup() {
 	createCanvas(windowWidth, windowHeight)
 
 	// context to use for loading blur shader
-	clockCanvas = createGraphics(windowWidth, windowHeight, WEBGL)
+	timeHolderCanvas = createGraphics(windowWidth, windowHeight, WEBGL)
 
-	// context (image) to be passed to shader
-	textCanvas = createGraphics(windowWidth, windowHeight, WEBGL)
+	// context (image) to be passed to blur shader
+	timeCanvas = createGraphics(windowWidth, windowHeight, WEBGL)
+
+	// context to use for loading noise shader
+	noiseHolderCanvas = createGraphics(windowWidth, windowHeight, WEBGL)
 
 	// fix for shader rendering flipped across y axis
-	textCanvas.scale(-1, 1)
+	timeCanvas.scale(-1, 1)
+
+	// disables scaling for retina screens which can create inconsistent scaling between displays
+	pixelDensity(1)
+
+	noiseHolderCanvas.pixelDensity(1)
+	noiseHolderCanvas.noStroke()
 
 	// TODO: move this to blur fn - needs to check if in process, etc
 	// tl.to(clockFace, {
@@ -125,24 +141,26 @@ function setup() {
 	// shakeFace()
 }
 
-function windowResized() {
-	resizeCanvas(windowWidth, windowHeight)
-}
-
 let cat = 50
 let runit = false
 
 function draw() {
-	// background(0)
+	// background of main canvas should be black
+	background(0)
+
+	severeLow()
+
+	blurFace()
 
 	// render clock face
 	clock()
 
 	// TODO: move draw cycle - needs to check if in process, etc
-	blurFace()
 
 	// TODO: move draw cycle - needs to check if in process, etc
 	// highAlarm()
+
+	// TODO: move draw cycle - needs to check if in process, etc
 
 	if (cat < 100 && runit) {
 		cat++
@@ -164,31 +182,38 @@ function clock() {
 	CLOCK_X = clockFace.x
 	CLOCK_Y = clockFace.y
 
+	// TODO: only do if severe high
+	// settings for clock background
+	// noiseHolderCanvas.rect(0, 0, width, height)
+
+	// // render noiseHolderCanvas to main canvas
+	// image(noiseHolderCanvas, 0, 0, windowWidth, windowHeight)
+
 	// settings for clock face
-	textCanvas.background(0);
-	textCanvas.textFont(clockFont)
-	textCanvas.textAlign(CENTER, CENTER)
-	textCanvas.textSize(width / 6)
+	timeCanvas.background('rgba(0, 0, 0, 0)');
+	timeCanvas.textFont(clockFont)
+	timeCanvas.textAlign(CENTER, CENTER)
+	timeCanvas.textSize(width / 6)
 
 	// text alpha 
-	textCanvas.fill(255, 0, 0, CLOCK_ALPHA)
+	timeCanvas.fill(255, 0, 0, CLOCK_ALPHA)
 
 	// text rotation
-	textCanvas.push()
-	textCanvas.rotate(CLOCK_ROTATION)
+	timeCanvas.push()
+	timeCanvas.rotate(CLOCK_ROTATION)
 
 	// get time
 	let time = getTime()
 
-	// write text to textCanvas
-	textCanvas.text(time, CLOCK_X, CLOCK_Y)
-	textCanvas.pop()
+	// write text to timeCanvas
+	timeCanvas.text(time, CLOCK_X, CLOCK_Y)
+	timeCanvas.pop()
 
 	// create geometry for text to render on
-	clockCanvas.rect(0, 0, windowWidth, windowHeight)
+	timeHolderCanvas.rect(0, 0, windowWidth, windowHeight)
 
-	// render clockCanvas to main canvas
-	image(clockCanvas, 0, 0, windowWidth, windowHeight)
+	// render timeHolderCanvas to main canvas
+	image(timeHolderCanvas, 0, 0, windowWidth, windowHeight)
 }
 
 function getTime() {
@@ -203,9 +228,35 @@ function getTime() {
 
 // ========== Effects ==========
 
-// function healSlower(params) {
+// function healRate() {
 
 // }
+
+function severeLow() {
+	// TODO: ability tp start and stop
+	// load shader to timeHolderCanvas
+	noiseHolderCanvas.shader(noiseShader)
+
+	// pass text canvas as image to shader
+	// (will be rendered on timeHolderCanvas)
+	// noiseShader.setUniform("tex0", timeCanvas)
+	noiseShader.setUniform("iResolution", [width, height])
+	noiseShader.setUniform("iFrame", frameCount)
+	// noiseShader.setUniform("iMouse", [mouseX, map(mouseY, 0, height, height, 0)])
+}
+
+function blurFace() {
+	CLOCK_BLUR = clockFace.blur
+
+	// TODO: shader stuff needs to happen regardless of blur state
+	// load shader to timeHolderCanvas
+	timeHolderCanvas.shader(blurShader)
+
+	// pass text canvas as image to shader
+	// (will be rendered on timeHolderCanvas)
+	blurShader.setUniform("tex0", timeCanvas)
+	blurShader.setUniform('texelSize', [(1.0 / width) / CLOCK_BLUR, (1.0 / height) / CLOCK_BLUR])
+}
 
 var osc
 var started = false
@@ -294,19 +345,6 @@ function driftFace() {
 	})
 }
 
-function blurFace() {
-	CLOCK_BLUR = clockFace.blur
-
-	// TODO: shader stuff needs to happen regardless of blur state
-	// load shader to clockCanvas
-	clockCanvas.shader(blurShader)
-
-	// pass text canvas as image to shader
-	// (will be rendered on clockCanvas)
-	blurShader.setUniform("tex0", textCanvas)
-	blurShader.setUniform('texelSize', [(1.0 / width) / CLOCK_BLUR, (1.0 / height) / CLOCK_BLUR])
-}
-
 function fadeOutFace() {
 	var tl = new TimelineLite({
 			repeat: 0
@@ -317,6 +355,10 @@ function fadeOutFace() {
 }
 
 // ========== Helpers ==========
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight)
+}
 
 // source: https://www.w3schools.com/js/js_random.asp
 function getRandInt(min, max) {
